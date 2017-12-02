8e6ccd110707cbbc1327022f52e30f55c33d8264
jamalam <jamalam-dev@outlook.com>
2017-12-02 08:47:44 -0700

    Put example code into main for high_resolution_clock dt calculation

--- source/main.cpp
+++ source/main.cpp
@@ -1,9 +1,30 @@
 #include "pid.h"
 #include <chrono>
+#include <stdio.h>
+#include <iostream>
+
 
 int main() {
 
+	using namespace std::chrono;
 
+	high_resolution_clock::time_point t1 = high_resolution_clock::now();
+
+	for (int i = 0; i < 1000; i++) {
+		printf("*");
+	}
+
+	printf("\n");
+
+	high_resolution_clock::time_point t2 = high_resolution_clock::now();
+
+	duration<double> timespan = duration_cast<duration<double>>(t2 - t1);
+
+	printf("dt = %1.10e\n", timespan.count());
+
+
+	int a;
+	std::cin >> a;
 
 	return 0;
 
